24ded19b2e596b81a51f908bda6be22aa1094467
stephen phillips <stephen.phillips@i3q.com>
2019-01-15 10:04:17 +0000

    Adding troposphere script to generate an ECS cluster rev 3

--- ecs-cluster-cf-template.py
+++ ecs-cluster-cf-template.py
@@ -104,7 +104,7 @@ t.add_resource(LaunchConfiguration(
 		" --region ",
 		Ref('AWS::Region'),
 		"\n"])),
-	ImageId='ami-04351e12',
+	ImageId='ami-d61027ad',
 	KeyName=Ref("KeyPair"),
 	SecurityGroups=[Ref("SecurityGroup")],
 	IamInstanceProfile=Ref('EC2InstanceProfile'),
