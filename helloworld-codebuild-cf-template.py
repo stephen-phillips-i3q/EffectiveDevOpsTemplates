cbcb32352b5ef07b527bfcbd735121d6a317caec
stephen phillips <stephen.phillips@i3q.com>
2019-01-22 17:28:28 +0000

    fixed issue with codebuild template

--- helloworld-codebuild-cf-template.py
+++ helloworld-codebuild-cf-template.py
@@ -57,24 +57,22 @@ environment = Environment(
 	],
 )
 
-buildspec = """
-version: 0.1
+buildspec = """version: 0.1
 phases:
 	pre_build:
 		commands:
 		- aws codepipeline get-pipeline-state --name "${CODEBUILD_INITIATOR##*/}" --query stageStates[?actionStates[0].latestExecution.externalExecutionId==\`$CODEBUILD_BUILD_ID\`].latestExecution.pipelineExecutionId --output=text > /tmp/execution_id.txt
-		- aws codepipeline get-pipeline-execution --pipeline-name "${CODEBUILD_INITIATOR##*/}" --pipeline-execution-id $(cat/tmp/execution_id.txt) --query 'pipelineExecution.artifactRevisions[0].revisionId' --output=text > /tmp/tag.txt
+		- aws codepipeline get-pipeline-execution --pipeline-name "${CODEBUILD_INITIATOR##*/}" --pipeline-execution-id $(cat /tmp/execution_id.txt) --query 'pipelineExecution.artifactRevisions[0].revisionId' --output=text > /tmp/tag.txt
 		- printf "%s:%s" "$REPOSITORY_URI" "$(cat /tmp/tag.txt)" > /tmp/build_tag.txt
 		- printf '{"tag":"%s"}' "$(cat /tmp/tag.txt)" > /tmp/build.json
 		- $(aws ecr get-login --no-include-email)
-
 	build:
 		commands:
 		- docker build -t "$(cat /tmp/build_tag.txt)" .
 	post_build:
 		commands:
 		- docker push "$(cat /tmp/build_tag.txt)"
-		- aws ecr batch-get-image --repository-name $REPOSITORY_NAME --imageids	imageTag="$(cat /tmp/tag.txt)" --query 'images[].imageManifest' --output text | tee /tmp/latest_manifest.json
+		- aws ecr batch-get-image --repository-name $REPOSITORY_NAME --image-ids imageTag="$(cat /tmp/tag.txt)" --query 'images[].imageManifest' --output text | tee /tmp/latest_manifest.json
 		- aws ecr put-image --repository-name $REPOSITORY_NAME --image-tag latest --image-manifest "$(cat /tmp/latest_manifest.json)"
 artifacts:
 	files: /tmp/build.json
